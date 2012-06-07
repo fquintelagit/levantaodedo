6485ec2b922e5c8f0c53cba37eca2363a269aba5
levantaodedo <fquintela@gmail.com>
2012-06-06 21:15:10 -0300

    Update master

--- server.js
+++ server.js
@@ -2,4 +2,4 @@ var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World\nApp (levantaodedo) is running on Node.JS ' + process.version);
-}).listen(process.env['app_port'] || 0);
+}).listen(process.env['app_port'] || 18397);
